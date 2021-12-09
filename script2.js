c0db794e3f955bbbed491a876130e25b93336975
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-09 15:01:18 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,23 @@
+
+const myAge = 22; {
+    if (myAge < 18) {
+        console.log('Sorry, you are too young');
+    } else {
+        console.log('Welcome and cheers');
+    }
+
+const isFemale = true;
+    if (isFemale == true)  {
+        console.log('Please come in mam');
+    }  else {
+        console.log('Sorry Sir, Ladies only');
+    }
+
+const driverStatus = 'bob';
+    if (driverStatus == 'bob')  {
+        console.log('You are allowed to drive');
+    }  else {
+        console.log('Sorry, you are pissed');
+    }
+
+ }
